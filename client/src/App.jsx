c032ff010001c7afc7aab6c10580a7d7295460bc
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-13 14:45:30 -0500

    fix userList rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import io from "socket.io-client"
 const socket = io.connect("http://localhost:3001")
 
 function App() {
 
-  const [userList, setUserList] = useState(["Pedro", "Alberto"])
+  const [userList, setUserList] = useState([{username: "Pedro"}, {username: "Alberto"}])
   const [username, setUsername] = useState("Example text")
 
   const sendMessage = () => {
-    socket.emit()
+    socket.emit(username)
   }
+
+  useEffect(() => {
+
+  }, [socket])
+
+  console.log(userList)
 
   return (
     <>
-      <input placeholder='Enter your name'></input>
+      <input placeholder='Enter your name' onChange={(e) => setUsername(e.target.value)}></input>
       <button onClick={sendMessage}>Enter</button>
       <div className='usernames'>
         {userList.map((user)=>{
-          <div>{user}</div>
+          return(<div>{user.username}</div>)
         })}
       </div>
     </>
