2fdc3f1ddf486ae769f4d35e4d64d5c818c7ed57
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-17 19:07:26 -0500

    update socket ngrok url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("https://d3e4b9a2785d.ngrok-free.app", {
+const socket = io("https://d89ac50e9ffa.ngrok-free.app", {
   transports: ['websocket']
 });
 export default socket;
