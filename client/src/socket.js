5aa03e4bdb2a42be719c3f0571de3d70569c6871
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-18 14:42:43 -0500

    ngrok-vercel test

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:3001", {
+const socket = io("https://1c8d9e87cb16.ngrok-free.app", {
   transports: ['websocket']
 });
 export default socket;
