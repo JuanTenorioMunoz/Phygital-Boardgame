bf1888da7cda6465335d41b8b031608c40a74989
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-24 09:17:29 -0500

    minor card fix

--- client/src/components/CharCard/CharCard.jsx
+++ client/src/components/CharCard/CharCard.jsx
@@ -1,13 +1,17 @@
-
-const CharCard = ({charName, onClick, image}) => {
-    return(
-        <>
-            <div className="character-container" onClick={(onClick)}>
-                <h1>{charName}</h1>
-                <img src={image}></img>
-            </div>
-        </>
-    )
+const CharCard = ({ charName, onClick, image }) => {
+  return (
+    <div 
+      className="character-container flex flex-col items-center cursor-pointer p-2" 
+      onClick={onClick}
+    >
+      <h1 className="text-lg font-bold">{charName}</h1>
+      <img 
+        src={image} 
+        alt={charName} 
+        className="w-60 h-60 object-contain rounded-3xl"
+      />
+    </div>
+  )
 }
 
 export default CharCard
--- client/src/components/HorizontalCard/HorizontalCard.jsx
+++ client/src/components/HorizontalCard/HorizontalCard.jsx
@@ -1,10 +1,10 @@
 import './HorizontalCard.css'
 
-const HorizontalCard = ({charName, onClick, credits, territories}) => {
+const HorizontalCard = ({charName, onClick, credits, image}) => {
     return(
         <>
             <div className="horizontal-card" onClick={(onClick)}>
-                <img className="character-image"></img>
+                <img className="character-image" src={image}></img>
                 <h1 className="character-name">{charName}</h1>
                 <h2 className="character-credits">{credits}</h2>
             </div>
