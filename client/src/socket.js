604728a7b9c3b71f850c4e60c7b1acae45b2101f
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-28 15:25:16 -0500

    vercel test

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("https://2e1910df8b8c.ngrok-free.app", {
+const socket = io("https://ef4c551b6015.ngrok-free.app", {
   transports: ['websocket']
 });
 export default socket;
