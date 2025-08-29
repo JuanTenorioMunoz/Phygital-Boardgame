a4365c6cd33141e93bcfb932e751c7ca9bc1e422
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-08-29 16:19:36 -0500

    vercel test

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
+import './Card.css'
+
 const Card = ({name, credits, desc}) => {
     return(
         <>
-            <div className="card">
+            <div className="territory-card">
                 <h1 className="name">{name}</h1>
                 <h2 className="desc">{desc}</h2>
                 <h3 className="credits">Credits: {credits}</h3>
