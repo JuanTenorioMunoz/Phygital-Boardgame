225a0e12622ef56998deadd2576173abba11168e
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-09-04 03:09:18 -0500

    .

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const socket = io("https://7c26925ddda8.ngrok-free.app", {
+const socket = io("https://7c26925ddda8.ngrok-free.awqepp", {
   transports: ['websocket']
 });
 export default socket;
