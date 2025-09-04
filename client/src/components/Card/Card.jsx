a8dea8c29afceea620554a66d8b846463b1135f3
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-09-04 02:13:48 -0500

    socket fix

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,11 +1,10 @@
 import './Card.css'
 
-const Card = ({name, credits, desc}) => {
+const Card = ({name, credits}) => {
     return(
         <>
             <div className="territory-card">
                 <h1 className="name">{name}</h1>
-                <h2 className="desc">{desc}</h2>
                 <h3 className="credits">Credits: {credits}</h3>
             </div>
         </>
