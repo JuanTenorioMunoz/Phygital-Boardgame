690e8cb697877141bb6a453887f814edb575c4b5
JuanTenorioMunoz <tenorio2003@gmail.com>
2025-09-04 02:08:24 -0500

    discard tailwind

--- client/src/components/CharCard/CharCard.jsx
+++ client/src/components/CharCard/CharCard.jsx
@@ -1,17 +1,12 @@
+import './CharCard.css'
+
 const CharCard = ({ charName, onClick, image }) => {
   return (
-    <div
-      className="character-container relative flex flex-col items-center cursor-pointer m-4" 
-      onClick={onClick}
-    >
-      <img
-        src={image}
-        alt={charName}
-        className="w-[160px] h-[220px] object-cover rounded-3xl" 
-      />
-      <h1 className="absolute bottom-3 left-1/2 -translate-x-1/2 text-xl font-extrabold tracking-wider text-white drop-shadow-lg">
-        {charName}
-      </h1>
+    <div className="character-container" onClick={onClick}>
+      <img src={image} alt={charName} className="character-image" />
+      <div className="character-name">
+        <h1>{charName}</h1>
+      </div>
     </div>
   )
 }
